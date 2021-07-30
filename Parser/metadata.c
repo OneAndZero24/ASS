5c87a5f3a4807e3cf5469421652806551dd4eb90
mateuuszzzzz <mati.icloud2000@gmail.com>
2021-07-30 21:58:01 +0200

    Added metadata.c

---
+++ Parser/metadata.c
@@ -0,0 +1,82 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *getFileType(const char *path) {
+
+    struct stat buf;
+
+    if( stat(path, &buf) == -1 ) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    int type = buf.st_mode & S_IFMT;
+
+    if(type == S_IFBLK)
+        return "block device";
+    else if(type == S_IFCHR)
+        return "character device";
+    else if(type == S_IFDIR)
+        return "directory";
+    else if(type == S_IFIFO)
+        return "FIFO/pipe";
+    else if(type == S_IFLNK)
+        return "symlink";
+    else if(type == S_IFREG)
+        return "regular file";
+    else if(type == S_IFSOCK)
+        return "socket";
+    else
+        return "unknown";
+}
+
+const char *getLastStatusChange(const char *path) {
+
+    struct stat buf;
+
+    if( stat(path, &buf) == -1 ) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    return ctime(&buf.st_ctime);
+}
+
+const char *getLastFileAccess(const char *path) {
+
+    struct stat buf;
+
+    if( stat(path, &buf) == -1 ) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    return ctime(&buf.st_atime);
+}
+
+const char *getLastFileModification(const char *path) {
+
+    struct stat buf;
+
+    if( stat(path, &buf) == -1 ) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    return ctime(&buf.st_mtime);
+}
+
+long long getFileSize(const char *path) {
+
+    struct stat buf;
+
+    if( stat(path, &buf) == -1 ) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    return (long long)buf.st_size;
+}
