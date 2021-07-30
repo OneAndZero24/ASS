88a41b62797955678a53ff4033448812099b131f
OneAndZero24 <oneandzero24@gmail.com>
2021-07-30 21:47:07 +0200

    image parser

--- Parser/fileMetadata.py
+++
@@ -1,4 +0,0 @@
-
-
-def get_file_type(path):
-    pass
--- Parser/imgParser.py
+++ Parser/imgParser.py
@@ -1,5 +1,13 @@
+from PIL import Image
+import numpy as np
+
 class ImgParser:
     def __init__(self):
         pass
-    def parse(self, path, size):
         
+    def parse(self, path, size):
+        arr = None
+        with Image.open(path).convert("RGB") as img:
+            img = img.resize(size)
+            arr = np.array(img)
+        return arr
