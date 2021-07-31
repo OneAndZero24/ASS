9dc4cf9b0ac69a5d81d3cef84057666ce9a9e712
OneAndZero24 <oneandzero24@gmail.com>
2021-07-31 13:46:53 +0200

    aaa

--- Model/FCM.py
+++ Model/FCM.py
@@ -1,9 +1,11 @@
+#WIP test model
 from .model import Model
 import numpy as np
 import skfuzzy
 
 class FCM(Model):
     def __init__(self):
-        
-    def run(self, mode=0):  # mode 0 - train/cluster, mode 1 - predict for given clustering 
-        pass
+
+    def fit(self, data):
+    
+    def run(self, data):
