35937291dc0d479509322dee0a38e0a18660ff38
OneAndZero24 <oneandzero24@gmail.com>
2021-07-31 11:58:35 +0200

    chuj

---
+++ tests/test-image_parser.py
@@ -0,0 +1,7 @@
+from ..Parser.parser import Parser
+from ..Parser.image_parser import ImageParser
+
+prs = Parser()  #test o chuj chodzi z polimorfizmem potem usune
+iprs = ImageParser((256, 256))
+prs = iprs
+print(prs.parse("data/Lenna.png"))
